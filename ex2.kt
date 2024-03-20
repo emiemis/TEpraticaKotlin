39461aa7b7a86841a30b256a2eb466d2e01a311c
Emilly Vetorazzi <emillybvetorazzi@gmail.com>
2024-03-20 09:22:15 -0300

    Declaração de três variáveis (inteiro, string, booleano) e atribuição de valores a elas. Em seguida, impressão dessas variáveis.

---
+++ ex2.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val nome = "Jonatan"
+    val idade = 18
+    val estudante = false
+
+    println("A pessoa $nome, de idade $idade, está declarado como $estudante no quesito de ser estudante")
+}
